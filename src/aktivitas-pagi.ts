26a7c5433a0aaf1e36d04522b5621fc85ab27bb0
Qmun14 <mamunramdhan@gmail.com>
2023-07-04 22:53:46 +0700

    Interface pada typescript

---
+++ src/aktivitas-pagi.ts
@@ -0,0 +1,6 @@
+/**
+ * ! Dalam interface setiap class yang mengimplement interface wajib mengoverride method2 yang ada di interface tersebut
+ */
+interface AktifitasPagi {
+  makan(): void;
+};
--- src/anak1.ts
+++ src/anak1.ts
@@ -1,12 +1,22 @@
 import { Induk } from "./induk";
 
-class Anak1 extends Induk {
+class Anak1 extends Induk implements AktifitasPagi, AktifitasSiang {
 
   getData2(): string {
     return "Data Abstract";
+  }
+
+  makan(): void {
+    console.log("Makan Pagi");
+  }
+
+  tidur(): void {
+    console.log("Tidur Siang");
   }
 }
 
 let a = new Anak1();
 console.log(a.getData());
 console.log(a.getData2());
+a.makan();
+a.tidur();
