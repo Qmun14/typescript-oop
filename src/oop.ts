f6e157ec18cff50853526179bcb26a1396aa2f2b
Qmun14 <mamunramdhan@gmail.com>
2023-07-04 12:16:58 +0700

    Constructor

--- src/oop.ts
+++ src/oop.ts
@@ -1,5 +1,22 @@
-class Induk {
+/**
+ *!  1. Contructor adalah metode khusus yang digunakan untuk menginisialisasi objek ketika objek kelas baru dibuat. Ini adalah metode kelas yang dieksekusi secara otomatis saat objek kelas baru dibuat dengan menggunakan kata kunci "new".
+ *!  2. Setiap class sebetulnya sudah ada default constructor nya, hanya saja default visible nya false
+ *!  3. ketika sebuah class anak membuat constructor sendiri wajib memanggil constructor induk nya menggunakan perintah super()
+ *!  4. Dalam typescript constructor dalam setiap class hanya boleh ada 1
+ *!  5. Dalam typescript tidak seperti java sebuah method/function tidak bisa di overloading
+ */
+
+class SuperInduk {
+
+}
+
+class Induk extends SuperInduk {
   namaInduk: string = "Ma'mun Ramdhan";
+
+  constructor(param1: string) {
+    super();
+    console.log('param1 dari anak adl', param1);
+  }
 
   getAlamat(): string {
     return "Jalan Induk Raya"
@@ -7,6 +24,11 @@ class Induk {
 }
 
 class Anak extends Induk {
+
+  constructor(param1: string) {
+    console.log(param1)
+    super(param1);
+  }
 
   getNamaInduk(): string {
     return this.namaInduk;
@@ -28,7 +50,7 @@ class Anak extends Induk {
   }
 }
 
-let a = new Anak();
+let a = new Anak("Anak1");
 console.log(a.getNamaInduk());
 console.log(a.getAlamat());
 console.log(a.getAlamatInduk());
